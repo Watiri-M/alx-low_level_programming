a05ae8b8e58d8414f3ef4719629e0b2ba87dfe00
Watiri-M <watirimungai3737@gmail.com>
2022-09-08 05:24:27 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
-/** * main - Entry point * * description: 'to display a string using the puts method' * * Return: Always 0 (Success) */
+/** * main - Entry point * * description: prints sentences * * Return: Programming is like building a multilingual puzzle */
 
 int main(void){	puts("\"Programming is like building a multilingual puzzle");	return (0);}
