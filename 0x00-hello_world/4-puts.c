1f690098d53c3659d45f16c441653cc10ae1cb6e
Watiri-M <watirimungai3737@gmail.com>
2022-09-08 04:27:31 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
- * Comments
  *
- * About programming
- */
+ *  * main 
+ *
+ *   *
+ *
+ *    * Return
+ *
+ *     */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
- 	Return(0)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
