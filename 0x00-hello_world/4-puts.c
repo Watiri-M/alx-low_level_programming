b0fac4063bb179e53ff697dbdbc49ad11f9de48e
Watiri-M <watirimungai3737@gmail.com>
2022-09-08 06:17:41 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -18,7 +18,5 @@ int main(void)
 
                         return (0);                                                                                                                                                                                                                                                                                      
 
-}                                                                                                                                                             
-
-^C
+}                                                                                       
 
