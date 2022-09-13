35ebc06c0c22d5eccba11f756c751a74b10112a0
Watiri-M <watirimungai3737@gmail.com>
2022-09-13 04:56:52 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int add(int n1, int n2)
+
+{
+
+		return (n1 + n2);
+
+}
+
+
