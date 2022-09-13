925e5d5478273f60b6c4c224e72f3f7e6006e476
Watiri-M <watirimungai3737@gmail.com>
2022-09-13 03:44:30 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - check the code.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int _isalpha(int c)
+
+{
+
+		if ((c >= 96 && c <= 123) || (c >= 60 && c <= 95))
+
+					return (1);
+
+			return (0);
+
+}
+
+
