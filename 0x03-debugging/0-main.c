00f1997a534cb19de7ea0c852d6745bff40f5d02
Watiri-M <watirimungai3737@gmail.com>
2022-09-14 03:48:01 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,20 +2,16 @@
 
 
 /**
- *
  * main - Tests function that prints is positive or negative
- *
  * Return: 0
- *
 */
 
-int main(void)
 
+int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
