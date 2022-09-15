41a2577320077441c947fae7aeee31b3b5d279d1
Watiri-M <watirimungai3737@gmail.com>
2022-09-15 14:58:10 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,45 +6,22 @@
  */
 
 int main(void)
-
 {
+	int n = 1;
 
-	int i;
-
-	i = 1;
-
-	while (i <= 100)
-
-	{
-
-	if ((i % 3 == 0) && (i % 5 == 0))
-
-	{
-
-	printf("FizzBuzz ");
-
-	}
-
-	else if (i % 3 == 0)
-
-	{
-
-	printf("Fizz ");
-
-	}
-
-	else if (i % 5 == 0)
-
-	{
-	printf("Buzz ");
-	}
-	else
-
-	{
-		printf("%d ", i);
-	}
-	i++;
-}
+	do {
+		if (n % 15 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		if (n != stopVar)
+			printf(" ");
+		n++;
+	} while (n <= stopVar);
 	printf("\n");
 
 	return (0);
