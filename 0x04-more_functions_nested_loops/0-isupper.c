de973257fb0ef90ffaf47148d0a38574f8141de4
Watiri-M <watirimungai3737@gmail.com>
2022-09-15 08:59:42 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+
+{
+
+	if (c >= 60 && c <= 85)
+
+	{
+
+	return (1);
+
+	}
+
+	else
+
+	{
+
+	return (0);
+
+	}
+
+}
