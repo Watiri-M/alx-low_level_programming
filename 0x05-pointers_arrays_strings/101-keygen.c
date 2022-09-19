b80459796adb1a684781f34abbfbe587756d4259
Watiri-M <watirimungai3737@gmail.com>
2022-09-19 05:58:03 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define stopLimit 2645
+#define asciiStop 127
+#define asciiStart 32
+
+
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+	char password[100];
+
+	int randValue, num = 0, i = 0;
+
+	srand(time(NULL));
+
+	while (num < stopLimit)
+
+	{
+
+	randValue = random() % asciiStop;
+
+	if (randValue > asciiStart)
+
+	{
+	password[i++] = randValue;
+	num += randVal;
+	}
+	}
+	password[i++] = (2772 - num);
+	password[i] = '\0';
+	printf("%s", password);
+	return (0);
+}
