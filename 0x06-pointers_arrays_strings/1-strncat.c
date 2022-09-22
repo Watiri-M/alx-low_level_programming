e5b1e861441b7e0f9842ab631fb33d6cbb086625
Watiri-M <watirimungai3737@gmail.com>
2022-09-22 04:23:31 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,23 +12,47 @@ char *_strncat(char *dest, char *src, int n)
 
 {
 
+	int len;
 	int i;
-	int destlen = 0;
-	int x = 0;
 
-	while (dest[x] != '\0')
+	len = get_len(dest);
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
 	{
-
-		destlen++;
-		x++;
+		dest[len + i] = src[i];
+		i++;
 
 	}
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-
-		dest[destlen + i] = src[i];
-		dest[destlen + i] = '\0';
+		dest[len + i] = '\0';
 
 	return (dest);
 
+}
+
+/**
+ * get_len - returns the length of a string
+ * @s: String character
+ * Return: length of string character
+ */
+
+int get_len(char *s)
+
+{
+
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		++len;
+		i++;
+
+	}
+
+	return (len);
 }
