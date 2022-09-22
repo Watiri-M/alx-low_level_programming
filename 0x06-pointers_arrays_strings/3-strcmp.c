84c5006af1ea13d4132f6eece4637b9c54966d26
Watiri-M <watirimungai3737@gmail.com>
2022-09-22 09:43:20 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+	int x = 0, y = 0;
+
+	while (x == 0)
+
+	{
+
+	if (*(s1 + y) == '\0' && *(s2 + y) == '\0')
+
+	break;
+
+	x = *(s1 + y) - *(s2 + y);
+
+	y++;
+
+	}
+
+	return (x)
+
+}
