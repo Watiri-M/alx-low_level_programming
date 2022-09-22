fa266eb20859a2a9233d4db53a0dd5f2e96618cf
Watiri-M <watirimungai3737@gmail.com>
2022-09-22 03:22:34 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: character
+ * @src: String character
+ * Return: A pointer to returning string
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+	int i;
+	int destLength = 0;
+	int counter = 0;
+
+	while (dest[counter] != '\0')
+
+	{
+	destLength++;
+
+	counter++;
+
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+
+	dest[destLength + i] = src[i];
+
+	dest[destLength + i] = '\0';
+
+	return (dest);
+}
