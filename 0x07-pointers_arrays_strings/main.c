409a9b2f63e6d69208c6ef52ae7f7a0c63df0936
Watiri-M <watirimungai3737@gmail.com>
2022-09-26 03:49:45 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Main
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	char *s = "main";
+	char *f = "testing";
+	unsigned int n;
+
+		i = _strspn(s, f);
+		printf("%u\n", n);
+	return (0);
+}
