704f088500e0dcf091871c8bf6b017f23332f242
Watiri-M <watirimungai3737@gmail.com>
2022-09-30 09:27:41 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: c1
+ * @argv: c2
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int x, y, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (x = 1; argv[x]; x++)
+	{
+		y = strtol(argv[x], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += y;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
