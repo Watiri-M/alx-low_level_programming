72fc7ee38616cd6401f2c17d75bcc7bc517d1312
Watiri-M <watirimungai3737@gmail.com>
2022-10-04 01:40:28 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,11 +9,10 @@
  */
 int _strlen(char *s)
 {
-	int size = 0;
-
-	for (; s[size] != '\0'; size++)
-	;
-	return (size);
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
 }
 
 /**
@@ -25,27 +24,28 @@ int _strlen(char *s)
  */
 char *argstostr(int ac, char **av)
 {
-	int i = 0, yz = 0, j = 0, count = 0;
-	char *s;
+int i = 0, nc = 0, j = 0, cmpt = 0;
+char *s;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+	return (NULL);
 
-	for (; i < ac; i++, yz++)
+for (; i < ac; i++, nc++)
+	nc += _strlen(av[i]);
 
-	s = malloc(sizeof(char) * yz + 1);
-	if (s == 0)
-		return (NULL);
+s = malloc(sizeof(char) * nc + 1);
+if (s == 0)
+	return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++, count++)
-			s[count] = av[i][j];
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+		s[cmpt] = av[i][j];
 
-		s[count] = '\n';
-		count++;
-	}
-	s[count] = '\0';
+	s[cmpt] = '\n';
+	cmpt++;
+}
+s[cmpt] = '\0';
 
-	return (s);
+return (s);
 }
