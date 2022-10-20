c66bf942b677a5b7d1fed0563b4189bb4e8a274f
Watiri-M <watirimungai3737@gmail.com>
2022-10-20 05:01:56 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * hare_toit - prints
+ *
+ * Return: void
+ */
+void hare_toit(void) __attribute__((constructor));
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
